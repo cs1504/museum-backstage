007e3d976cb31013719b500ce3aa082270f05f19
0xl2oot <0xl2oot@gmail.com>
2018-04-03 01:32:34 +0800

    update

--- application/api/controller/NewsControlApi.php
+++ application/api/controller/NewsControlApi.php
@@ -44,18 +44,18 @@ class NewsControlApi extends Controller
                 $news = Db::query("select id, title, author, release_time, modify_time, excerpt, content, status, nature, img_url from news where (`title` like '%".$data['title']."%') limit ".$data['page']." , 10");
             }
             else if(isset($data['key'])) {
-                $list = Jieba::cut($data["key"]);
-                $q = '';
-                foreach ($list as $ss) {
-                    if (strlen($ss) > 1) {
-                        $q .= str_replace('%','',urlencode($ss)) . ' ';
-                    }
-                }
+//                $list = Jieba::cut($data["key"]);
+//                $q = '';
+//                foreach ($list as $ss) {
+//                    if (strlen($ss) > 1) {
+//                        $q .= str_replace('%','',urlencode($ss)) . ' ';
+//                    }
+//                }
                 if(!isset($data['page']))
                     $data['page'] = 1;
                 $data['page'] = ($data['page']-1) * 10;
-                $news = Db::query("select id, title, author, release_time, modify_time, excerpt, content, status, nature from news where MATCH (titleindex,excerptindex,contentindex) AGAINST ('+".$q."') limit ".$data['page']." , 10");
-//                $news = Db::query("select id, title, author, release_time, modify_time, excerpt, content, status, nature, img_url from news where (`title` like '%".$data['key']."%') or (`content` like '%".$data['key']."%') limit ".$data['page']." , 10");
+//                $news = Db::query("select id, title, author, release_time, modify_time, excerpt, content, status, nature from news where MATCH (titleindex,excerptindex,contentindex) AGAINST ('+".$q."') limit ".$data['page']." , 10");
+                $news = Db::query("select id, title, author, release_time, modify_time, excerpt, content, status, nature, img_url from news where (`title` like '%".$data['key']."%') or (`content` like '%".$data['key']."%') limit ".$data['page']." , 10");
             }
             else {
                 if(!isset($data['page']))
