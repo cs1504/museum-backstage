d4edb19db07e6af8de836b1a0181f84f8e1389f3
0xl2oot <0xl2oot@gmail.com>
2018-04-18 22:00:07 +0800

    api

--- application/api/controller/ExhibitionControlApi.php
+++ application/api/controller/ExhibitionControlApi.php
@@ -2,8 +2,6 @@
 
 namespace app\api\controller;
 
-
-
 use think\Controller;
 use app\api\model\Exhibition;
 use think\Db;
@@ -13,7 +11,12 @@ class ExhibitionControlApi extends Controller
     public function exhibition($id)
     {
         if($this->request->isGet()) {
-            $exhibition = Exhibition::where('id', $id)->find();
+            $exhibition = Db::table('exhibition')
+                ->alias('e')
+                ->where('e.id', $id)
+                ->join('museum m', 'e.museum_id = m.id')
+                ->field('e.id, e.name as title, m.name as museum, m.id as museum_id, e.time, e.address, e.introduce')
+                ->find();
             if(!$exhibition) {
                 return json(['valid'=>0,'msg'=>'没有此展览']);
             }
@@ -39,12 +42,19 @@ class ExhibitionControlApi extends Controller
             $data = input('get.');
             if(!isset($data['page']))
                 $data['page'] = 1;
+//            $exhibition = Db::table('exhibition')
+//                ->alias('e')
+//                ->where('e.name', 'like', '%'.$data['name'].'%')
+//                ->join('museum m', 'e.museum_id = m.id')
+//                ->page($data['page'], 10)
+//                ->field('e.id, e.name, m.name as museum, m.id as museum_id, e.time, e.address, e.introduce')
+//                ->select();
             $exhibition = Db::table('exhibition')
                 ->alias('e')
                 ->where('e.name', 'like', '%'.$data['name'].'%')
                 ->join('museum m', 'e.museum_id = m.id')
                 ->page($data['page'], 10)
-                ->field('e.id, e.name, m.name as museum, m.id as museum_id, e.time, e.address, e.introduce')
+                ->field('e.id, e.name as title, m.name as museum, m.id as museum_id, e.time, e.address, e.introduce')
                 ->select();
             return json($exhibition);
         }
